030250eec9e61ae9776d2de7cf62dea0f584fe10
Takanori Saito <odinsphere57@gmail.com>
2021-11-30 00:11:25 +0900

    ログイン画面を簡単にデザインしてみる。

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>test</div>
+    <v-card class="mx-auto" max-width="344" light>
+      <v-card-title>ログイン</v-card-title>
+      <v-card-subtitle
+        >ユーザIDとパスワードを入力してください。</v-card-subtitle
+      >
+      <v-text-field class="mx-4"></v-text-field>
+      <v-text-field class="mx-4"></v-text-field>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn small>OK</v-btn>
+        <v-btn small>Cancle</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
